323deacb8a1dde07046c6c4c2a6f82963fc7a979
renan veezor <renan.balbino56@gmail.com>
2025-09-26 19:03:55 -0300

    add pred, even, odd

--- Solutions/FMCBabyNat.hs
+++ Solutions/FMCBabyNat.hs
@@ -39,14 +39,20 @@ isZero (S _) = O
 
 -- pred is the predecessor but we define zero's to be zero
 pred :: Nat -> Nat
-pred = undefined
+pred O = O
+pred (S n) = n
 
 -- Output: O means False, S O means True
 even :: Nat -> Nat
-even = undefined
+even O = S O
+even (S O) = O
+even (S(S n)) = even n 
 
 odd :: Nat -> Nat
-odd = undefined
+odd O = O
+odd (S O) = S O
+odd (S(S n)) = odd n
+ 
 
 -- This is called the dotminus or monus operator
 -- (also: proper subtraction, arithmetic subtraction, ...).
